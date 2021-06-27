72d3b01709081ab27230860370ba53a716c881fb
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-06-27 18:57:12 +0700

    [Added] Locally Netlify functions w/ Netlify dev

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,10 +1,11 @@
-exports.handlers = async () => {
+exports.handler = async () => {
   console.log("function ran");
 
   const data = { name: "mario", age: 35, job: "plumber" };
 
   // return response to browser
   // upload this to netlify to get an api endpoint
+  // use netlify cli to spinup a proxy server at port 8888 ( run netlify dev)
   return {
     statusCode: 200,
     body: JSON.stringify(data),
